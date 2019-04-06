dc5c18cf377d254c8f28b528f1cd2d6565096457
Liders Toro <liderz250@gmail.com>
2019-04-05 23:16:20 -0400

    commit dos del modelo MVC

---
+++ WebApplication1/WebApplication1/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿namespace WebApplication1.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TipoEstado
+    {
+        Activo,
+        Inactivo
+
+    }
+    public enum Unidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        [Display(Name="Nombre del Producto")]
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+
+        [Required]
+        public Unidad Unidad{ get; set; }
+        [Required]
+        public TipoEstado Estado { get; set; }
+
+    }
+}
